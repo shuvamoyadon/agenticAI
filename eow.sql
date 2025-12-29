cb75bd966bc7b74dc9b524c18714f98007273b9e
shuvamoyadon <42385240+shuvamoyadon@users.noreply.github.com>
2025-12-29 10:21:03 +0530

    Update eow.sql

--- eow.sql
+++ eow.sql
@@ -1,18 +1,69 @@
-UPDATE edp-dev-storage.edp_ent_cma_plss_onboarding_src.T_PCT_GCP_FIN_PRODUCT_CONFIGURATION
-SET
-  payload = (
-    SELECT ARRAY_AGG(
-      CASE
-        WHEN JSON_VALUE(elem, '$.Id') = 'a09bc00000CCJOSAA5'
-        THEN JSON_SET(
-               elem,
-               '$.AttributeName__c', 'NEW_ATTRIBUTE_VALUE'
-             )
-        ELSE elem
-      END
-    )
-    FROM UNNEST(payload) AS elem
-  ),
-  indicator_flag = 'Y'
-WHERE ID = 'a01bc00000UuQWOAA3'
-  AND indicator_flag = 'N';
+INSERT INTO `playground-s-11-443f35ef.test.T_PCT_GCP_FIN_PRODUCT_SRC` 
+(
+  ID,
+  Product_Association_ID,
+  line_of_business,
+  Grouper_val,
+  CreatedDate,
+  LastModifiedDate,
+  inserted_time,
+  updated_time,
+  indicator_flag,
+  Payload
+)
+VALUES
+(
+  'a09bc00000C9dkrAAB',
+  'a01bc00000UuJxzAAF',
+  'Caremark',
+  'TestingGrouper1225',
+  TIMESTAMP('2025-12-22T18:15:02.000Z'),
+  TIMESTAMP('2025-12-22T18:15:02.000Z'),
+  CURRENT_TIMESTAMP(),
+  CURRENT_TIMESTAMP(),
+  'N',
+  PARSE_JSON("""
+  [
+    {
+      "AttributeName__c": "Billing_Model",
+      "Attribute_Category__c": "Billing",
+      "Attribute_Definition__c": "0tjc0000001ddAAA",
+      "Attribute_Value__c": "Not Selected",
+      "Client_External_Id__c": "AccExtIdHC601225",
+      "Configured_Product__c": "0QLbc000002E4x3GAC",
+      "CreatedDate": "2025-12-22T18:15:02.000Z",
+      "Grouper_ID__c": "TestingGrouper1225",
+      "Id": "a09bc00000C9dkrAAB",
+      "LastModifiedBy": {
+        "Name": {
+          "value": "PCT GCP Integration User"
+        }
+      },
+      "LastModifiedDate": "2025-12-22T18:15:02.000Z",
+      "Name": "PCH-151528",
+      "Product_Association_ID": "a01bc00000UuJxzAAF",
+      "line_of_business": "Caremark"
+    },
+    {
+      "AttributeName__c": "Notes_Paid_For_By_Other_Selected",
+      "Attribute_Category__c": "Billing",
+      "Attribute_Definition__c": "0tjc0000001fAAA",
+      "Attribute_Value__c": null,
+      "Client_External_Id__c": "AccExtIdHC601225",
+      "Configured_Product__c": "0QLbc000002E4x3GAC",
+      "CreatedDate": "2025-12-22T18:15:02.000Z",
+      "Grouper_ID__c": "TestingGrouper1225",
+      "Id": "a09bc00000C9dksAAB",
+      "LastModifiedBy": {
+        "Name": {
+          "value": "PCT GCP Integration User"
+        }
+      },
+      "LastModifiedDate": "2025-12-22T18:15:02.000Z",
+      "Name": "PCH-151529",
+      "Product_Association_ID": "a01bc00000UuJxzAAF",
+      "line_of_business": "Caremark"
+    }
+  ]
+  """)
+);
