cb3e7803388a9f56278ab10882df418b5f487b23
shuvamoyadon <42385240+shuvamoyadon@users.noreply.github.com>
2025-12-31 10:36:34 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,147 +1,110 @@
-
-MERGE INTO `playground-s-11-443f35ef.test.CUSTOMER_PRODUCT_CONFIGURATIONS` T
-USING (
-  -- ==========================================
-  -- SOURCE DATA 
-  -- ==========================================
-  WITH exploded AS (
-    SELECT
-      line_of_business AS business_unit_cd,
-      ID,
-      Product_Association_ID,
-      JSON_QUERY_ARRAY(Payload) AS payload_array
-    FROM `playground-s-11-443f35ef.test.T_PCT_GCP_FIN_PRODUCT_SRC`
-    WHERE active_indicator_flag = 'Y'
-  ),
-
-  flattened AS (
-    SELECT
-      business_unit_cd,
-      ID,
-      Product_Association_ID,
-      REGEXP_REPLACE(
-        JSON_VALUE(element, '$.Attribute_Category__c'),
-        r'\\u0026amp;|&amp;',
-        '&'
-      ) AS client_category,
-      element AS json_object
-    FROM exploded
-    CROSS JOIN UNNEST(payload_array) AS element
-  ),
-
-  agg AS (
-    SELECT
-      business_unit_cd,
-      ID AS sf_product_configuration_id,
-      Product_Association_ID AS sf_product_association_id,
-
-      TO_JSON(ARRAY_AGG(IF(client_category = 'Product Level', json_object, NULL) IGNORE NULLS)) AS product_level,
-      TO_JSON(ARRAY_AGG(IF(client_category = 'Client Type', json_object, NULL) IGNORE NULLS)) AS client_type,
-      TO_JSON(ARRAY_AGG(IF(client_category = 'Billing', json_object, NULL) IGNORE NULLS)) AS billing_detail,
-      TO_JSON(ARRAY_AGG(IF(client_category = 'Program Rules', json_object, NULL) IGNORE NULLS)) AS program_rules,
-      TO_JSON(ARRAY_AGG(IF(client_category = 'Assigned Owner & Contact Information', json_object, NULL) IGNORE NULLS)) AS assigned_owner_contact_info,
-      TO_JSON(ARRAY_AGG(IF(client_category = 'Client Contract', json_object, NULL) IGNORE NULLS)) AS client_contract,
-      TO_JSON(ARRAY_AGG(IF(client_category = 'Account Management', json_object, NULL) IGNORE NULLS)) AS account_management,
-      TO_JSON(ARRAY_AGG(IF(client_category = 'ROI/PGs', json_object, NULL) IGNORE NULLS)) AS ROI_performance_guarantees,
-      TO_JSON(ARRAY_AGG(IF(client_category = 'Implementation', json_object, NULL) IGNORE NULLS)) AS product_implementation,
-      TO_JSON(ARRAY_AGG(IF(client_category = 'Reporting', json_object, NULL) IGNORE NULLS)) AS reporting_detail
-    FROM flattened
-    GROUP BY
-      business_unit_cd,
-      ID,
-      Product_Association_ID
-  ),
-
-  -- ==========================================
-  -- DUPLICATE STREAM FOR INSERT
-  -- ==========================================
-  scd_source AS (
-    SELECT
-      'EXPIRE' AS action_type,
-      *
-    FROM agg
-
-    UNION ALL
-
-    SELECT
-      'INSERT' AS action_type,
-      *
-    FROM agg
-  )
-
-  SELECT * FROM scd_source
-) S
-
-ON T.sf_product_configuration_id = S.sf_product_configuration_id
-AND T.active_record_ind = 'Y'
-AND S.action_type = 'EXPIRE'
-
--- EXPIRE CURRENT ACTIVE RECORD
-WHEN MATCHED
-AND (
-  TO_JSON_STRING(T.product_level) != TO_JSON_STRING(S.product_level)
-  OR TO_JSON_STRING(T.client_type) != TO_JSON_STRING(S.client_type)
-  OR TO_JSON_STRING(T.billing_detail) != TO_JSON_STRING(S.billing_detail)
-  OR TO_JSON_STRING(T.program_rules) != TO_JSON_STRING(S.program_rules)
-  OR TO_JSON_STRING(T.assigned_owner_contact_info) != TO_JSON_STRING(S.assigned_owner_contact_info)
-  OR TO_JSON_STRING(T.client_contract) != TO_JSON_STRING(S.client_contract)
-  OR TO_JSON_STRING(T.account_management) != TO_JSON_STRING(S.account_management)
-  OR TO_JSON_STRING(T.ROI_performance_guarantees) != TO_JSON_STRING(S.ROI_performance_guarantees)
-  OR TO_JSON_STRING(T.product_implementation) != TO_JSON_STRING(S.product_implementation)
-  OR TO_JSON_STRING(T.reporting_detail) != TO_JSON_STRING(S.reporting_detail)
+## change the Attribute_Category__c and check
+INSERT INTO `playground-s-11-d5e75d75.test.T_PCT_GCP_FIN_PRODUCT_CONFIGURATION`
+(
+  ID,
+  Product_Association_ID,
+  line_of_business,
+  Grouper_val,
+  CreatedDate,
+  LastModifiedDate,
+  inserted_time,
+  updated_time,
+  indicator_flag,
+  Payload
 )
-THEN
-  UPDATE SET
-    T.active_record_ind = 'N',
-    T.updated_timestamp = CURRENT_TIMESTAMP()
-
--- INSERT NEW VERSION
-WHEN NOT MATCHED
-AND S.action_type = 'INSERT'
-THEN
-  INSERT (
-    sgk_customer_product_configuration_id,
-    sf_product_configuration_id,
-    sf_product_association_id,
-    business_unit_cd,
-    product_level,
-    client_type,
-    billing_detail,
-    program_rules,
-    assigned_owner_contact_info,
-    client_contract,
-    account_management,
-    ROI_performance_guarantees,
-    product_implementation,
-    reporting_detail,
-    active_record_ind,
-    created_timestamp,
-    updated_timestamp
-  )
-  VALUES (
-    GENERATE_UUID(),
-    S.sf_product_configuration_id,
-    S.sf_product_association_id,
-    S.business_unit_cd,
-    S.product_level,
-    S.client_type,
-    S.billing_detail,
-    S.program_rules,
-    S.assigned_owner_contact_info,
-    S.client_contract,
-    S.account_management,
-    S.ROI_performance_guarantees,
-    S.product_implementation,
-    S.reporting_detail,
-    'Y',
-    CURRENT_TIMESTAMP(),
-    CURRENT_TIMESTAMP()
-  );
-
-
-
-UPDATE `playground-s-11-443f35ef.test.T_PCT_GCP_FIN_PRODUCT_SRC` SRC
-SET active_indicator_flag = 'N'
-FROM `playground-s-11-443f35ef.test.CUSTOMER_PRODUCT_CONFIGURATIONS` TGT
-WHERE SRC.ID = TGT.sf_product_configuration_id
-and src.active_indicator_flag = 'Y' and TGT.active_record_ind='Y';
+VALUES
+(
+  'a09bc00000C9dkrAAB',
+  'a01bc00000UuJxzAAF',
+  'Caremark',
+  'TestingGrouper1225',
+  TIMESTAMP('2025-12-22T18:15:02.000Z'),
+  TIMESTAMP('2025-12-22T18:15:02.000Z'),
+  CURRENT_TIMESTAMP(),
+  CURRENT_TIMESTAMP(),
+  'N',
+  JSON '''
+  [
+    {
+      "AttributeName__c": "Billing Model",
+      "Attribute_Category__c": "Client Contract",
+      "Attribute_Definition__c": "0tjbce000001dAAA",
+      "Attribute_Value__c": "PMPM (per member per month)",
+      "Client_External_Id__c": "785469",
+      "Configured_Product__c": "0QLbce00002FMALG4",
+      "CreatedDate": "2025-12-24T13:05:58.000Z",
+      "Grouper_ID__c": "0QLbce00002FMALG4",
+      "Id": "a09bc00000CWD3AA5",
+      "LastModifiedBy": {
+        "Name": {
+          "value": "Anupama Retnadev"
+        }
+      },
+      "LastModifiedDate": "2025-12-24T13:05:58.000Z",
+      "Name": "PCH-151915",
+      "Product_Association_ID": "a01bce0000ZLnvAAF",
+      "line_of_business": "Third Party"
+    },
+    {
+      "AttributeName__c": "Notes Paid For By Other Selected",
+      "Attribute_Category__c": "Client Contract",
+      "Attribute_Definition__c": "0tjbce000001fAAA",
+      "Attribute_Value__c": null,
+      "Client_External_Id__c": "785469",
+      "Configured_Product__c": "0QLbce00002FMALG4",
+      "CreatedDate": "2025-12-24T13:05:58.000Z",
+      "Grouper_ID__c": "0QLbce00002FMALG4",
+      "Id": "a09bc00000CWDKAA5",
+      "LastModifiedBy": {
+        "Name": {
+          "value": "Anupama Retnadev"
+        }
+      },
+      "LastModifiedDate": "2025-12-24T13:05:58.000Z",
+      "Name": "PCH-151916",
+      "Product_Association_ID": "a01bce0000ZLnvAAF",
+      "line_of_business": "Third Party"
+    },
+    {
+      "AttributeName__c": "Contracted LOB Sub Category",
+      "Attribute_Category__c": "Client Contract",
+      "Attribute_Definition__c": "0tjbce000001gAAA",
+      "Attribute_Value__c": "Commercial Fully Insured;FTI;Med EGP Group;Med EGP Individual",
+      "Client_External_Id__c": "AccExtInde2323",
+      "Configured_Product__c": "0QLbce00002GYFugAO",
+      "CreatedDate": "2025-12-26T16:00:04.000Z",
+      "Grouper_ID__c": "wegwe",
+      "Id": "a09bc00000CF1GAA1",
+      "LastModifiedBy": {
+        "Name": {
+          "value": "PCT GCP Integration User"
+        }
+      },
+      "LastModifiedDate": "2025-12-26T16:00:04.000Z",
+      "Name": "PCH-152069",
+      "Product_Association_ID": "a01bce00004dqaAAB",
+      "line_of_business": "Caremark"
+    },
+    {
+      "AttributeName__c": "Notes_CoBranding_Level_Specific_Explanation",
+      "Attribute_Category__c": "Client Contract",
+      "Attribute_Definition__c": "0tjbce000002EJAA",
+      "Attribute_Value__c": null,
+      "Client_External_Id__c": "AccExtInde2323",
+      "Configured_Product__c": "0QLbce00002GYFugAO",
+      "CreatedDate": "2025-12-26T16:00:04.000Z",
+      "Grouper_ID__c": "wegwe",
+      "Id": "a09bc00000CF1GAA4",
+      "LastModifiedBy": {
+        "Name": {
+          "value": "PCT GCP Integration User"
+        }
+      },
+      "LastModifiedDate": "2025-12-26T16:00:04.000Z",
+      "Name": "PCH-152070",
+      "Product_Association_ID": "a01bce00004dqaAAB",
+      "line_of_business": "Caremark"
+    }
+  ]
+  '''
+);
